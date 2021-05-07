d7e167c70a123c9f3229cc1b231f0abfb350e5ad
Eldar Tengizov <madpunch@mail.ru>
2021-05-07 19:09:14 +0300

    Initial Commit

---
+++ Spotify UI/Spotify_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_UIApp.swift
+//  Spotify UI
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
